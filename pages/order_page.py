68771fc377618c43e4766feddadff1e02108bd74
Alexander Cheremisov <dadic2169@mail.ru>
2024-10-26 23:07:00 +0300

    modifieds files order_page.py and personal_page.py

--- pages/order_page.py
+++ pages/order_page.py
@@ -7,47 +7,46 @@ import allure
 class OrderPage(BasePage):
 
     @allure.step('Открываем страницу Ленты заказов')
-    def go_to_order_feed_page(self):
+    def open_order_feed(self):
         self.navigate_to(Urls.ORDER_LIST_URL)
 
     @allure.step('Проверяем открытие страницы Ленты заказов')
-    def check_page(self):
+    def verify_order_feed_page(self):
         return self.verify_page_opened(Urls.ORDER_LIST_URL, OrdersFeedLocators.ORDERS_FEED_HEADER)
 
     @allure.step('Кликаем на последний заказ в списке')
-    def click_last_order(self):
+    def select_last_order(self):
         self.is_element_invisible(OrderFeedLocators.LOADING_ICON)
         self.click_element(OrdersFeedLocators.MOST_RECENT_ORDER)
 
     @allure.step('Проверяем наличие модального окна с деталями заказа')
-    def check_open_order_detail_modal_window(self):
+    def is_order_detail_modal_visible(self):
         return self.is_element_visible(OrdersFeedLocators.ORDER_CONTENT_LABEL)
 
     @allure.step('Получаем номер последнего заказа в ленте')
-    def get_order_number(self):
+    def fetch_last_order_number(self):
         return self.get_text(OrdersFeedLocators.MOST_RECENT_ORDER_ID)
 
     @allure.step('Получаем значение счетчика "Выполнено за все время"')
-    def get_completed_for_all_time_counter(self):
+    def fetch_total_completed_counter(self):
         return self.get_text(OrdersFeedLocators.TOTAL_COMPLETED_COUNTER)
 
     @allure.step('Получаем значение счетчика "Выполнено за сегодня"')
-    def get_executed_today_counter(self):
+    def fetch_today_completed_counter(self):
         return self.get_text(OrdersFeedLocators.TODAY_COMPLETED_COUNTER)
 
     @allure.step('Ожидаем и получаем отображение номера заказа "В работе"')
-    def get_order_number_in_the_work_list(self):
+    def fetch_order_number_in_progress(self):
         if self.wait_for_text_change(OrdersFeedLocators.ACTIVE_ORDER_LIST_ITEMS, 'Все текущие заказы готовы!'):
             return self.get_text(OrdersFeedLocators.ACTIVE_ORDER_LIST_ITEMS)
         else:
             self.print_error(f'Проблема: отображается {OrdersFeedLocators.ACTIVE_ORDER_LIST_ITEMS}')
 
     @allure.step('Проверяем открытие страницы истории заказов')
-    def check_history_page(self):
+    def verify_order_history_page(self):
         return self.verify_page_opened(Urls.USER_ORDER_HISTORY_URL, UserProfileLocators.ORDER_HISTORY_PAGE_LINK)
 
     @allure.step('Получаем номер первого заказа в истории')
-    def get_first_order_number(self):
+    def fetch_first_order_number(self):
         return self.get_text(OrderHistoryLocators.INITIAL_ORDER_ID)
-
 
--- pages/personal_page.py
+++ pages/personal_page.py
@@ -1,25 +1,26 @@
 from base_page import BasePage
 from locators import *
-from config import *
+from config import Urls
 import allure
 
+class PersonalPage(BasePage):
 
-class PersonalAreaPage(BasePage):
+    @allure.step('Открываем Личный кабинет')
+    def open_personal_account(self):
+        self.navigate_to(Urls.USER_ACCOUNT_URL)
 
-    @allure.step(f'Открываем Личный кабинет')
-    def go_to_personal_area_page(self):
-        self.go_to_url(Urls.ACCOUNT_URL)
-
-    @allure.step(f'Проверяем открытие страницы с Личным кабинетом')
-    def check_page(self):
-        return self.check_open_page(Urls.PROFILE_URL, PersonalAreaPageLocators.PROFILE_LINK)
+    @allure.step('Проверяем открытие страницы с Личным кабинетом')
+    def verify_personal_account_page(self):
+        return self.verify_page_opened(Urls.USER_PROFILE_URL, UserProfileLocators.PROFILE_PAGE_LINK)
 
     @allure.step('Нажимаем на ссылку "История заказов"')
-    def click_order_history_link(self):
-        self.is_invisible_element(PersonalAreaPageLocators.LOADING_ANIMATION)
-        self.click_on_element_with_wait(PersonalAreaPageLocators.ORDER_HISTORY_LINK)
+    def access_order_history(self):
+        self.is_element_invisible(UserProfileLocators.LOADING_ICON)
+        self.click_element(UserProfileLocators.ORDER_HISTORY_PAGE_LINK)
 
     @allure.step('Нажимаем на ссылку "Выход"')
-    def click_exit_link(self):
-        self.is_invisible_element(PersonalAreaPageLocators.LOADING_ANIMATION)
-        self.click_on_element_with_wait(PersonalAreaPageLocators.LOGOUT_BUTTON)
+    def logout(self):
+        self.is_element_invisible(UserProfileLocators.LOADING_ICON)
+        self.click_element(UserProfileLocators.LOGOUT_BUTTON)
+
+
